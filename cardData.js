776964dddb65012fdacb17369c11d63dfabe8100
Lu <luanths@users.noreply.github.com>
2020-07-05 18:33:18 -0400

    Add card set to embed

--- cardData.js
+++ cardData.js
@@ -70,14 +70,39 @@ const cardtypes = [
   "Spell"
 ];
 
+const cardsets = {
+  10000: "Basic",
+  10001: "Classic",
+  10002: "Darkness Evolved",
+  10003: "Rise of Bahamut",
+  10004: "Tempest of the Gods",
+  10005: "Wonderland Dreams",
+  10006: "Starforged Legends",
+  10007: "Chronogenesis",
+  10008: "Dawkbreak Nightedge",
+  10009: "Brigade of the Sky",
+  10010: "Omen of the Ten",
+  10011: "Altersphere",
+  10012: "Steel Rebellion",
+  10013: "Rebirth of Glory",
+  10014: "Verdant Conflict",
+  10015: "Ultimate Colosseum",
+  10016: "World Uprooted",
+  10017: "Fortune's Hand",
+  10018: "Set 18",
+  10019: "Set 19",
+  90000: "Token",
+};
+
 function fillEmbed(embed, card) {
   let cost = card.cost;
   let craft = crafts[card.clan];
   let rarity = rarities[card.rarity - 1];
   let cardtype = cardtypes[card.char_type - 1];
   let trait = card.tribe_name;
+  let cardset = cardsets[card.card_set_id];
   embed.setTitle(card.card_name);
-  embed.setDescription(`${cost}pp ${craft} ${rarity} ${cardtype}\n${trait}`);
+  embed.setDescription(`${cost}pp ${craft} ${rarity} ${cardtype} | ${trait}`);
   if (cardtype === "Follower") {
     let text = card.skill_disc.replace(/<br>/g, "\n");
     let evo_text = card.evo_skill_disc.replace(/<br>/g, "\n");
@@ -85,8 +110,9 @@ function fillEmbed(embed, card) {
     embed.addField("Evolved", `${card.evo_atk}/${card.evo_life}\n${evo_text}`);
   } else {
     let text = card.skill_disc.replace(/<br>/g, "\n");
-    embed.addField("Base", text);
+    embed.addField(cardtype, text);
   }
+  embed.addField("Set", cardset);
 }
 
 module.exports = { update, get, find, fillEmbed };
