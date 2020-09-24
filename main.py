c1f363166dbb1af02c6105334a55ea0b2f2a2b60
Lu <luanths@users.noreply.github.com>
2020-09-23 21:26:37 -0400

    Simplifications

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import discord
 from discord.ext import commands
 
 import card_data
-#import card_art
+import card_art
 import deck_code
 
 command_prefix = os.environ["BOT_PREFIX"].split()
@@ -36,24 +36,6 @@ async def on_ready():
         activity = discord.Game(name=f"{command_prefix[0]}help")
         await bot.change_presence(activity=activity)
 
-@bot.command()
-async def greet(ctx):
-    msg = await ctx.send('Send me that ▶️ reaction, mate')
-    await msg.add_reaction('▶️')
-
-    def check(reaction, user):
-        return user != bot.user and reaction.message.id == msg.id and str(reaction.emoji) == '▶️'
-
-    counter = 0
-    while(True):
-        try:
-            await bot.wait_for('reaction_add', timeout=60.0, check=check)
-        except asyncio.TimeoutError:
-            break
-            await msg.clear_reactions()
-        else:
-            counter += 1
-            await msg.edit(content="nice"+str(counter))
 
 @bot.command()
 async def invite(ctx):
@@ -82,86 +64,49 @@ async def cards(ctx, *query):
     if not results:
         raise CardNotFoundError(query)
     else:
-        counter = 0
         max_results = 20
-        lines = [f"Found {len(results)} cards:"]
-        lines += [card_data.effective_card_name(card) for card in results[:max_results]]
-        lines += [
-            f"(displaying {counter*max_results+1}-{min(len(results), counter*max_results+max_results)} of {len(results)})"
-        ]
-
-        msg = await ctx.send("\n".join(lines))
-
-        if len(results) > max_results:
+        if len(results) <= max_results:
+            lines = [f"Found {len(results)} cards:"]
+            lines += [card_data.effective_card_name(card) for card in results]
+            await ctx.send("\n".join(lines))
+        else:
+            lo = 0
+            hi = max_results
+            lines = [f"Found {len(results)} cards, displaying {lo+1}-{hi}:"]
+            lines += [card_data.effective_card_name(card) for card in results[lo:hi]]
+            msg = await ctx.send("\n".join(lines))
+            await msg.add_reaction("⬅️")
             await msg.add_reaction("➡️")
 
             def check(reaction, user):
                 return (
                     user != bot.user
                     and reaction.message.id == msg.id
-                    and (str(reaction.emoji) == "➡️" or str(reaction.emoji) == "⬅️")
+                    and reaction.emoji in ["➡️", "⬅️"]
                 )
-
-            max_counter = int(len(results) / max_results)
-            if len(results) % max_results == 0:
-                max_counter -= 1
 
             while True:
                 try:
-                    Reaction, usr = await bot.wait_for(
+                    reaction, usr = await bot.wait_for(
                         "reaction_add", timeout=30.0, check=check
                     )
-                    # prevReact = discord.Reaction
                 except asyncio.TimeoutError:
-                    await msg.clear_reaction("➡️")
-                    await msg.clear_reaction("⬅️")
+                    await msg.remove_reaction("➡️", bot.user)
+                    await msg.remove_reaction("⬅️", bot.user)
                     break
                 else:
-                    if Reaction.emoji == "➡️":
-                        counter += 1
-                        lines = [
-                            card_data.effective_card_name(card)
-                            for card in results[
-                                counter * max_results : counter * max_results
-                                + max_results
-                            ]
-                        ]
-                        lines += [
-                            f"(displaying {counter*max_results+1}-{min(len(results), counter*max_results+max_results)} of {len(results)})"
-                        ]
-                        if len(lines) > 0:
-                            await msg.edit(content="\n".join(lines))
+                    if reaction.emoji == "➡️":
+                        hi = min(len(results), hi + max_results)
+                        lo = hi - max_results
+                    elif reaction.emoji == "⬅️":
+                        lo = max(0, lo - max_results)
+                        hi = lo + max_results
 
-                        if counter > 0:
-                            await msg.add_reaction("⬅️")
-
-                        if counter >= max_counter:
-                            await msg.clear_reaction("➡️")
-
-                    elif Reaction.emoji == "⬅️":
-                        counter -= 1
-                        lines = [
-                            card_data.effective_card_name(card)
-                            for card in results[
-                                counter * max_results : counter * max_results
-                                + max_results
-                            ]
-                        ]
-
-                        if (
-                            counter * max_results + max_results < len(results)
-                            and len(results[counter * max_results + max_results :]) > 0
-                        ):
-                            await msg.add_reaction("➡️")
-
-                        if counter >= 0:
-                            lines += [
-                                f"(displaying {counter*max_results+1}-{min(len(results), counter*max_results+max_results)} of {len(results)})"
-                            ]
-                            await msg.edit(content="\n".join(lines))
-
-                        if counter == 0:
-                            await msg.clear_reaction("⬅️")
+                    lines = [f"Found {len(results)} cards, displaying {lo+1}-{hi}:"]
+                    lines += [
+                        card_data.effective_card_name(card) for card in results[lo:hi]
+                    ]
+                    await msg.edit(content="\n".join(lines))
 
 
 @bot.command(aliases=["c", "text", "t"])
